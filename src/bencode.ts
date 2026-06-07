f3093ecaad28b45f737bdc911e3c8f7aaad9e0f9
qyrhx <qyrhx@disroot.org>
2026-06-07 02:45:44 +0200

    Full encoder implementation.

--- src/bencode.ts
+++ src/bencode.ts
@@ -1,17 +1,20 @@
-type BencodeElem = BencodeStr | BencodeNum | BencodeDict | BencodeList;
+type BencodeElem = BencodeStr | BencodeInt | BencodeDict | BencodeList;
 type BencodeStr = string;
-type BencodeNum = number;
+type BencodeInt = number;
 type BencodeDict = Map<BencodeElem, BencodeElem>;
 type BencodeList = BencodeElem[];
 
 export function bencode_decode(bstr: string): BencodeElem {
-  const [res, _] = bencode_decode_next_elem(bstr, 0);
+  const [res, l] = bencode_decode_next_elem(bstr, 0);
+  if (l !== bstr.length) {
+    throw new Error("trailing data");
+  }
   return res;
 }
 
 function bencode_decode_next_elem(bstr: string, pos: number): [BencodeElem, number] {
   if (bstr.charAt(pos) === "i") {
-    return bencode_decode_num(bstr, pos);
+    return bencode_decode_int(bstr, pos);
   }
   else if (bstr.charAt(pos) === "d") {
     return bencode_decode_dict(bstr, pos);
@@ -27,7 +30,7 @@ function bencode_decode_next_elem(bstr: string, pos: number): [BencodeElem, numb
   }
 }
 
-function bencode_decode_num(bstr: string, pos: number): [BencodeNum, number] {
+function bencode_decode_int(bstr: string, pos: number): [BencodeInt, number] {
   if (bstr.charAt(pos) !== "i") {
     throw new Error("could not find number to decode");
   }
@@ -36,7 +39,7 @@ function bencode_decode_num(bstr: string, pos: number): [BencodeNum, number] {
     throw new Error("unterminated integer");
   }
   const ntxt = bstr.slice(pos + 1, end);
-  if (!/^-?\d+$/.test(ntxt)) {
+  if (!/^(0|(-?[1-9]\d*))$/.test(ntxt)) {
     throw new Error("invalid bencode number");
   }
   return [Number(ntxt), end+1]
@@ -53,6 +56,9 @@ function bencode_decode_str(bstr: string, pos: number): [BencodeStr, number] {
   }
   const len = Number(text);
   const str = bstr.slice(mid+1, mid+len+1);
+  if (str.length !== len) {
+    throw new Error("Unexpected EOF");
+  }
   return [str, mid+len+1];
 }
 
@@ -63,8 +69,19 @@ function bencode_decode_dict(bstr: string, pos: number): [BencodeDict, number] {
   let res: BencodeDict = new Map();
   let p = pos+1;
   let k, v: BencodeElem;
+  let prevk: BencodeElem = "";
   while (bstr.charAt(p) !== "e") {
     [k, p] = bencode_decode_next_elem(bstr, p);
+    if (!isBencodeStr(k)) {
+      throw new Error("map key is not a string");
+    }
+    if (res.has(k)) {
+      throw new Error("duplicate key");
+    }
+    if (k < prevk) {
+      throw new Error("keys not ordered");
+    }
+    prevk = k;
     [v, p] = bencode_decode_next_elem(bstr, p);
     res.set(k, v);
   }
@@ -83,4 +100,8 @@ function bencode_decode_list(bstr: string, pos: number): [BencodeList, number] {
     res.push(elem);
   }
   return [res, p+1];
+}
+
+function isBencodeStr(x: BencodeElem): boolean {
+  return typeof x === "string";
 }
